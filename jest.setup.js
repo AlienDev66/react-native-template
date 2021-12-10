7838a33d6e863edf44b20185f000385aa1b512d6
Domingos Capitango <59874434+AlienDev66@users.noreply.github.com>
2021-12-10 17:44:05 +0100

    Initial commit

---
+++ jest.setup.js
@@ -0,0 +1,24 @@
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import { NativeModules } from 'react-native';
+
+NativeModules.ReactLocalization = { language: 'en' };
+
+require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+jest.mock('react-native-bootsplash', () => ({
+  hide: jest.fn().mockResolvedValueOnce(),
+  show: jest.fn().mockResolvedValueOnce(),
+  getVisibilityStatus: jest.fn().mockResolvedValue('hidden'),
+}));
+
+jest.mock('react-native-config', () => ({
+  Config: {
+    API_BASE_URL: 'XXX',
+    BUILD_VARIANT: 'TEST',
+  },
+}));
+
+// Silence the warning: Animated: `useNativeDriver` is not supported
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
